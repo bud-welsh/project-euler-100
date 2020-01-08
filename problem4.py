fbfbfe1bf2a3833bd0ca01114d0f54d53debecac
Bud Welsh <budwelsh14@gmail.com>
2020-01-07 22:19:10 -0600

    Not right yet

--- problem4.py
+++ problem4.py
@@ -1,21 +1,46 @@
 # A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 
-first_number = 99
+first_number = 91
 second_number = 99
-number = first_number * second_number
-string_number = str(number)
-list_number = list(string_number)
-reverse_list = list(reversed(list_number))
-print(str(first_number) + '*' + str(second_number) + '=' + str(number))
+answer = 1
+reverse_answer = 0
+remainder = 0
+store = 0
 
-while list_number != reverse_list:
-  if list_number == reverse_list:
-    print(str(first_number) + '*' + str(second_number) + '=' + str(number))
-  else:
-    if first_number >= 10:
-      first_number = first_number - 1
-      print(str(first_number) + '*' + str(second_number) + '=' + str(number))
-    else:
-      first_number = 99
-      second_number = second_number - 1
-      print(str(first_number) + '*' + str(second_number) + '=' + str(number))
+
+
+
+while reverse_answer != answer:
+  answer = first_number * second_number 
+  store = answer
+
+  while store > 0:
+    reverse_answer = reverse_answer * 10
+    remainder = store % 10
+    store = ((store - remainder) / 10)
+    reverse_answer = reverse_answer + remainder
+    print(answer)
+    print(reverse_answer)
+    print(first_number)
+    print(second_number)
+
+  first_number += 1
+  print(first_number)
+
+#   print(answer)
+#   print(reverse_answer)
+
+#   if first_number > 0:
+#     first_number = first_number - 1
+#     if first_number == 1:
+#       first_number = 99
+#       second_number = second_number - 1
+#       if second_number == 1:
+#         first_number = 91
+#         second_number = 99
+#       else:
+#         pass
+#     else:
+#       pass
+#   else:
+#     pass
