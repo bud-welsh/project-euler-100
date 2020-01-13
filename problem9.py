bcc5afa7314d667cc131b94fc2f5d8b0c4f75e2e
Bud Welsh <budwelsh14@gmail.com>
2020-01-13 17:31:10 -0600

    Problem 9 solution

--- problem9.py
+++ problem9.py
@@ -2,8 +2,8 @@
 
 for x in range(1, 1000):
     for y in range(1, 1000):
-        for z in range(1, 1000):
-            if ((x ** 2) + (y ** 2) == (z ** 2)):
-                print('%d * %d * %d = %d' % (x, y, z, x * y * z))
+        z = 1000 - x - y
+        if ((x ** 2) + (y ** 2) == (z ** 2)):
+            print('%d * %d * %d = %d' % (x, y, z, x * y * z))
 
 print("Done")
